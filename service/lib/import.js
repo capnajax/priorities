7043ba1af1ed82bde832ac468b41e9f89dedd7b1
Thomas Wilkinson <thomas.wilkinson@us.ibm.com>
2019-03-04 16:21:15 -0600

    Improved encapsulation, import dependencies

--- service/lib/import.js
+++ service/lib/import.js
@@ -13,169 +13,242 @@ const
 	MODEL_TASK = 'task',
 	MODEL_SUBTASK = 'subtask';
 
-var idMap = {}, // maps titles or references to ids
-	models;
-
-function collectPromises(promises) { return new Promise((resolve, reject) => {
-	Promise.all(promises)
-	.then(results => {
-			resolve(results);
-		})
-	.catch(reason => {
-			reject(reason);
-		});
-})}
-
-function importNote (workItemId, sequence, noteData) { return new Promise((resolve, reject) => {
-
-	debug("Importing note", JSON.stringify(noteData.note));
-
-	Promise.resolve()
-	.then(() => {
-			return models.Note.create({title: noteData.note, workItemId, sequence});
-		})
-	.then(note => {
-			resolve(note);
-			debug("Imported Note:");
-			debug(note);
-		})
-	.catch(reason => {
-			var failure = {
-					status: "Failure",
-					message: "Failed to import note \"" + noteData.title + "\"",
-					note: noteData.title,
-					reason: reason
-				};
-			debug(failure);
-			reject(failure);
-		});
-});}
-
-function importItem (_parentId, _priority, _taskLevel, _itemData) { return new Promise((resolve, reject) => {
-
-	var importedItem,
-		levelName = c.TASKLEVELS[_taskLevel].item;
-
-	debug("Importing item _taskLevel", _taskLevel, "content:", JSON.stringify(_itemData.subtask));
-
-	Promise.resolve()
-	.then(() => {
-			// check projects have no parent
-			if (_taskLevel === c.TASKLEVEL_PROJECT) {
-				if (_parentId) {
-					return Promise.reject({
-						code: "ERROR_PROJECT_WITH_PARENT",
-						message: "projects should not have parents"
-					});
-				}
-			}
-		})
-	.then(() => {
-			return models.WorkItem.create({	
-					name: _itemData[levelName],
-					taskLevel: _taskLevel,
-					parentId: _parentId,
-					priority: _priority,
-					isComplete: _itemData.complete || false
-				})
-				.then(_item => {
-					let key = _itemData.ref || _itemData[levelName];
-					if (_.has(idMap, key)) {
-						return Promise.reject({
-							code: "ERROR_DUPLICATE_REFERENCE",
-							message: "more than one item with a title or ref " + key,
-							details: {
-									key,
-									ids: [_item.id, idMap[key].id]
-								}
-						});
-					}
-					idMap[key] = _item.id;
-					console.log("Mapping", levelName, key, "to", idMap[key]);
-					return _item;
-				});
-		})
-	.then(_item => {
-			importedItem = _item;
-			debug("Imported", levelName+':');
-			debug(importedItem);
-
-			let children = c.TASKLEVELS[_taskLevel].children,
-				childPromises = [];
-
-			childPromises.push(_itemData.notes 
-				? _.map(_itemData.notes, (_s,_i) => {return importNote(_item.id,_i+1,_s)}) 
-				: []);
-
-			for (let i in children) {
-				let childLevel = c.TASKLEVELS[children[i]];
-				childPromises.push(_itemData[childLevel.list]
-					? _.map(_itemData[childLevel.list], (_n,_i) => {
-							return importItem(_item.id,_i+1,children[i],_n);
-						})
-					: []);
-			}
-
-			return collectPromises.apply(null, childPromises);
-		})
-	.then(_promises => resolve(importedItem))
-	.catch(reason => {
-			var failure = {
-					status: "Failure",
-					message: "Failed to import " + levelName + " \"" + _itemData[levelName] + "\"",
-					subtask: _itemData,
-					reason: reason
-				};
-			debug(failure);
-			reject(failure);
-		});
-});}
-
-function importProjects(projectsData) { return new Promise((resolve, reject) => {
-
-	debug("importProjects started. projectsData:");
-	debug(projectsData);
-
-	let projectPromises = [],
-		warnings = [],
-		errors = [];
-
-	// import
-
-	debug("importing");
-	projectPromises = projectsData.projects 
-		? _.map(projectsData.projects, (p,i) => {return importItem(null, i+1, c.TASKLEVEL_PROJECT, p)})
-		: [];
-	debug('projectPromises');
-	debug(projectPromises);
-
-	Promise.resolve()
-	.then(() => {
-			return errors.length ? Promise.reject(errors) : Promise.resolve();
-		})
-	.then(() => {
-			return collectPromises(projectPromises);
-		})
-	.then(() => {
-			result = {status: "imported"};
-			warnings && _.extend(result, {warnings});
-			errors   && _.extend(result, {errors});
-			resolve(result);
-		})
-	.catch(reason => {
-			debug("Error importing:", reason);
-			debug.enabled && debug(JSON.stringify(reason));
-			reject({
-				code: "ERRORS_ON_IMPORT",
-				messages: "Failed to import project set",
-				details: reason
-			});
-		});
-})};
-
 function importYaml(appModels) {
 
-	var startTime = new Date().getTime();
+	var startTime = new Date().getTime(),
+		idMap = {}, // maps titles or references to ids
+		dependencies = {},
+		models,
+
+		importDependencies = () => { return new Promise((resolve, reject) => {
+
+				debug("importDependencies called");
+				debug("dependencies:");
+				debug(dependencies);
+
+				var unknownIds = [];
+
+				// first scan the id map
+				for (let i in dependencies) {
+					for (let j in dependencies[i]) {
+						if (!_.has(idMap, dependencies[i][j])) {
+							console.log("Unknown ref in dependencies for", i, ":", j);
+							unknownIds.push(j);
+						}
+					}
+				}
+
+				if (unknownIds.length) {
+
+					reject({
+						code: "ERROR_UNKNOWN_DEPENDENCY",
+						message: "unknown dependencies: " + JSON.stringify(unknownIds) 
+					});
+
+				} else {
+
+					// add the dependencies
+					let importObjects = [];
+					for (let i in dependencies) {
+						for (let j in dependencies[i]) {
+							importObjects.push({
+								dependencyId: idMap[dependencies[i][j]],
+								dependentId: idMap[i]
+							});
+						}
+					}
+
+					models.Dependency.create(importObjects)
+					.then(() => {
+						resolve();
+					})
+					.catch(reason => {
+						reject({
+							code: "ERROR_FAILED_DEPENDENCY",
+							message: "Failed to import dependencies",
+							details: reason
+						});
+					});
+				}
+
+			})},
+
+		importItem = (_parentId, _priority, _taskLevel, _itemData) => { return new Promise((resolve, reject) => {
+
+			var levelName = c.TASKLEVELS[_taskLevel].item;
+
+			debug("Importing item _taskLevel", _taskLevel, "content:", JSON.stringify(_itemData.subtask));
+
+			Promise.resolve()
+			.then(() => {
+					// check projects have no parent
+					if (_taskLevel === c.TASKLEVEL_PROJECT) {
+						if (_parentId) {
+							return Promise.reject({
+								code: "ERROR_PROJECT_WITH_PARENT",
+								message: "projects should not have parents"
+							});
+						}
+					}
+				})
+			.then(() => {
+					return models.WorkItem.create({	
+							name: _itemData[levelName],
+							taskLevel: _taskLevel,
+							parentId: _parentId,
+							priority: _priority,
+							isComplete: _itemData.complete || false
+						})
+						.then(_item => {
+
+							let key = _itemData.ref || _itemData[levelName];
+
+							// map the name (or ref) in the yaml to an actual id
+							if (_.has(idMap, key)) {
+								return Promise.reject({
+									code: "ERROR_DUPLICATE_REFERENCE",
+									message: "more than one item with a title or ref " + key,
+									details: {
+											key,
+											ids: [_item.id, idMap[key].id]
+										}
+								});
+							}
+							idMap[key] = _item.id;
+							debug("Mapping", levelName, key, "to", idMap[key]);
+
+							// capture the dependencies this has.
+							if (_.has(_itemData, "depends")) {
+								dependencies[key] = _.map(
+										_itemData.depends, 
+										depend => { return depend.depend; }
+									);
+							}
+
+							return _item;
+						});
+				})
+			.then(_item => {
+					debug("Imported", levelName+':');
+					debug(_item);
+
+					let children = c.TASKLEVELS[_taskLevel].children,
+						childPromises = [];
+
+					if (_itemData.notes) {
+						_itemData.notes.forEach((_s,_i) => {
+							childPromises.push(importNote(_item.id,_i+1,_s))
+						});
+					}
+
+					for (let i in children) {
+						let childLevel = c.TASKLEVELS[children[i]];
+						if (_itemData[childLevel.list]) {
+							_itemData[childLevel.list].forEach((_n,_i) => {
+								childPromises.push(importItem(_item.id,_i+1,children[i],_n));
+							});
+						}
+					}
+
+					return Promise.all(childPromises);
+				})
+			.then(_promises => {
+					// now we need to import the dependencies
+					return _promises;
+				})
+			.then(() => {
+					debug("importItem resolve");
+					resolve();
+				})
+			.catch(reason => {
+					var failure = {
+							status: "Failure",
+							message: "Failed to import " + levelName + " \"" + _itemData[levelName] + "\"",
+							subtask: _itemData,
+							reason: reason
+						};
+					debug(failure);
+					reject(failure);
+				});
+			});},
+
+		importNote = (workItemId, sequence, noteData) => { return new Promise((resolve, reject) => {
+
+			debug("Importing note", JSON.stringify(noteData.note));
+
+			Promise.resolve()
+			.then(() => {
+					return models.Note.create({title: noteData.note, workItemId, sequence});
+				})
+			.then(note => {
+					debug("Imported Note:");
+					debug(note);
+					debug("importNote resolve");
+					resolve(note);
+				})
+			.catch(reason => {
+					var failure = {
+							status: "Failure",
+							message: "Failed to import note \"" + noteData.title + "\"",
+							note: noteData.title,
+							reason: reason
+						};
+					debug(failure);
+					reject(failure);
+				});
+			});},
+
+		importProjects = (projectsData) => { return new Promise((resolve, reject) => {
+
+			debug("importProjects started. projectsData:");
+			debug(projectsData);
+
+			let projectPromises = [],
+				warnings = [],
+				errors = [];
+
+			// import
+
+			debug("importing");
+			projectPromises = projectsData.projects 
+				? _.map(projectsData.projects, (p,i) => {
+						return importItem(null, i+1, c.TASKLEVEL_PROJECT, p);
+					})
+				: [];
+			debug('projectPromises');
+			debug(projectPromises);
+
+			Promise.resolve()
+			.then(() => {
+					return errors.length ? Promise.reject(errors) : Promise.resolve();
+				})
+			.then(() => {
+					debug("Collecting promises");
+					return Promise.all(projectPromises).then(() => { 
+							debug("all resolve");
+						});
+				})
+			.then(() => {
+					debug("Ready to import dependencies");
+					return importDependencies();
+				})
+			.then(() => {
+					debug("Dependencies imported");
+					result = {status: "imported"};
+					warnings && _.extend(result, {warnings});
+					errors   && _.extend(result, {errors});
+					resolve(result);
+				})
+			.catch(reason => {
+					debug("Error importing:", reason);
+					debug.enabled && debug(JSON.stringify(reason));
+					reject({
+						code: "ERRORS_ON_IMPORT",
+						messages: "Failed to import project set",
+						details: reason
+					});
+				});
+			})};
 
 	debug("importYaml called, appModels ==", appModels);
 	models = appModels;
